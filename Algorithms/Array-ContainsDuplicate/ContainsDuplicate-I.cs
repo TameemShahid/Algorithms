1dcf618578791913f496b76387d8dda1127adf66
Tameem Shahid <muhammad.tameemshahid@kineticsoftware.com>
2021-12-30 00:09:30 +0500

    Array-ContainsDuplicate: Added solution

---
+++ Algorithms/Array-ContainsDuplicate/ContainsDuplicate-I.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Array_ContainsDuplicate
+{
+    class ContainsDuplicate_I
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> elements = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool added = elements.Add(nums[i]);
+                if (!added) return true;
+            }
+            return false;
+        }
+    }
+}
